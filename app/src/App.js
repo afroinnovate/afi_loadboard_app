0ae8aa154e2d780b3c1c804ec4f067d12e6d6c97
Tango Tew <tango.tew@outlook.com>
2023-10-12 00:18:06 -0500

    first app scaffold

--- app/src/App.js
+++ app/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Welcome to load board landing page
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://home.afroinnovate.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Afroinnovate Mission Statement and values
         </a>
       </header>
     </div>
