ee2835cfa58055f85c34ae0f87de383b0260a8ea
Tango Tew <tango.tew@outlook.com>
2023-11-06 15:50:03 -0600

    enable the deployment and refactor the code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
+  return [{ title: 'LoadBoard APP' }, { name: 'description', content: 'Welcome to afroinnovate loadboard app' }];
 };
 
 export default function Index() {
