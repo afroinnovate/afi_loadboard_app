1efeb00dcfb3fd79819ade6b9a9274bfe978528a
tew9 <tango.tew1@accesscfa.com>
2024-01-25 17:39:35 -0600

    try fix for the login

--- app/api/services/session.ts
+++ app/api/services/session.ts
@@ -4,7 +4,7 @@ import { createCookieSessionStorage } from "@remix-run/node";
 const secretKeyDevelopment = process.env.SECRET_KEY_DEV || '';
 const secretKeyProduction = process.env.SECRET_KEY_PROD || '';
 
-console.log("secretKeyDevelopment", secretKeyDevelopment);
+// console.log("secretKeyDevelopment", secretKeyDevelopment);
 // Choose the secret based on the environment
 const secretKey = process.env.NODE_ENV === "production" ? secretKeyProduction : secretKeyDevelopment;
 
@@ -12,7 +12,7 @@ const secretKey = process.env.NODE_ENV === "production" ? secretKeyProduction : 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "_session", // use any name you want here
-    domain:"localhost", //change it to afroinnovate.com when you deploy
+    domain:"afroinnovate.com", //change it to afroinnovate.com when you deploy
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
