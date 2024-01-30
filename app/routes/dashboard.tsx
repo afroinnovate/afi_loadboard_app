5fe4a5a8029395e0aa658d5ea23b2361a6109bc5
tew9 <tango.tew1@accesscfa.com>
2024-01-30 08:59:31 -0600

    add role control on carrier dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -7,10 +7,10 @@ import type {
 } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import customStyles from "../styles/global.css";
-import { authenticator } from "~/api/services/auth.server";
-import { getSession } from "~/api/services/session";
-import Sidebar from "~/components/sidebar";
-import Overview from '~/components/overview';
+import { authenticator } from "../api/services/auth.server";
+import { getSession } from "../api/services/session";
+import Sidebar from "../components/sidebar";
+import Overview from '../components/overview';
 
 export const meta: MetaFunction = () => {
   return [
@@ -43,55 +43,93 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Dashboard() {
-  const loaderData = useLoaderData();
+  const loaderData: any  = useLoaderData();
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const toggleSidebar = () => setSidebarOpen(!sidebarOpen); 
   const location = useLocation();
   
   const isLoadOperationsActive = location.pathname.startsWith('/dashboard/loads/');
-  
-  console.log("dashboard logging loader data", loaderData);
-  return (
-    <div>
-      <header className="flex justify-between items-center py-4 px-8 bg-gray-100 border-b-2 border-gray-200">
-        <div className="flex items-center space-x-4">
-          <button onClick={toggleSidebar} className="text-black hover:text-black mr-4 text-4xl">
-            {/* Replace with an appropriate icon or text */}
-            &#9776;
-          </button>
-          <NavLink
-            to="/dashboard/"
-            end
-            className={({ isActive }) =>
-              "text-black font-semibold " + (isActive ? "border-b-2 border-blue-400" : "text-gray-400 hover:text-black")
-            }
-          >
-            Home
-          </NavLink>
+  const user = loaderData.user;
+  // const user: any = {
+  //   "id": "228e82ac-ed1a-47a6-8027-9a6fc0a0efb9",
+  //   "userName": "tangogatdet76@gmail.com",
+  //   "email": "tangogatdet76@gmail.com",
+  //   "firstName": "Tango",
+  //   "lastName": "Tew",
+  //   "roles": [
+  //       "OWNER_OPERATOR",
+  //       "owner_operator"
+  //   ]
+  // }
 
-          <NavLink
-            to="/dashboard/loads/view/"
-            className={() =>
-              "text-black font-semibold " + (isLoadOperationsActive ? "border-b-2 border-blue-400" : "text-gray-500 hover:text-black")
-            }
-          >
-            Load Operations
-          </NavLink>
+  const roles = user.roles.map((role: string) => role.toLowerCase());
 
+  // Check if user has 'support', 'admin' or any role containing 'carrier'
+  const hasAccess = roles.includes('support') || roles.includes('admin') || roles.some(role => role.includes('carrier'));
+
+
+  console.log("dashboard logging loader data", user);
+  if (!hasAccess) {
+    return (
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+        <div className="bg-white py-8 px-6 shadow-2xl rounded-lg sm:px-10 max-w-2xl">
+          <h1 className="text-center text-2xl font-extrabold text-red-700 py-4">
+            Access Denied!
+          </h1>
+          <p className="text-center text-gray-700 mb-4 italic">
+            You do not have access to carrier dashboard.
+          </p>
+          <div className="flex justify-center">
+            <Link to="/" className="primary-action">
+              Go Back Home 
+            </Link>
+          </div>
         </div>
-        <Link to="/dashboard/help" className="text-gray-500 hover:text-black px-4 py-2 rounded hover:border-b-2 hover:border-blue-400">
-          Help
-        </Link>
-      </header>
-      <div className="flex">
-        {sidebarOpen && <Sidebar />}
-        <main className='flex-1 justify-center content-center p-5 shadow-lg'>
-          { location.pathname === '/dashboard/' && (
-            <Overview />
-          )}
-          <Outlet />
-        </main>
       </div>
-    </div>
-  );
+    );
+  }else {
+    return (
+      <>
+        <header className="flex justify-between items-center py-4 px-8 bg-gray-100 border-b-2 border-gray-200">
+          <div className="flex items-center space-x-4">
+            <button onClick={toggleSidebar} className="text-black hover:text-black mr-4 text-4xl">
+              {/* Replace with an appropriate icon or text */}
+              &#9776;
+            </button>
+            <NavLink
+              to="/dashboard/"
+              end
+              className={({ isActive }) =>
+                "text-black font-semibold " + (isActive ? "border-b-2 border-blue-400" : "text-gray-400 hover:text-black")
+              }
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              to="/dashboard/loads/view/"
+              className={() =>
+                "text-black font-semibold " + (isLoadOperationsActive ? "border-b-2 border-blue-400" : "text-gray-500 hover:text-black")
+              }
+            >
+              Load Operations
+            </NavLink>
+
+          </div>
+          <Link to="/dashboard/help" className="text-gray-500 hover:text-black px-4 py-2 rounded hover:border-b-2 hover:border-blue-400">
+            Help
+          </Link>
+        </header>
+        <div className="flex">
+          {sidebarOpen && <Sidebar />}
+          <main className='flex-1 justify-center content-center p-5 shadow-lg'>
+            { location.pathname === '/dashboard/' && (
+              <Overview />
+            )}
+            <Outlet />
+          </main>
+        </div>
+      </>
+    );
+  }
 }
